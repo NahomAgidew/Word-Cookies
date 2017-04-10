8293b3ab19e5afc080c915d0c098b9de556f6a13
Nahom Abi <nahomagidew@gmail.com>
2017-04-09 19:45:31 -0700

    Initial Commit

---
+++ word_cookies.py
@@ -0,0 +1,25 @@
+"""
+	A script to solve the game "Word Cookies" by
+	calculating the permutation of the letters and
+	finding a valid English word from that.
+	
+	Author: Nahom Abi
+	Email: nahomagidew@gmail.com
+"""
+
+from itertools import permutations
+from PyDictionary import PyDictionary
+
+dictionary = PyDictionary()
+
+letters = raw_input('Input Letters:').split(',')
+length = int(raw_input('Input length of word:'))
+perms = permutations(letters, length)
+
+for perm in perms:
+	word = ''.join(perm)
+	try:
+		meaning = dictionary.meaning(word)
+		print 'Found word:', word
+	except Error as e:
+		pass
